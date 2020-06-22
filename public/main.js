85baadf4e2469e13a2fd3ef1a799d51a55e86f0b
TakLee96 <jiahang_li@outlook.com>
2020-06-22 06:33:26 +0000

    configure login and basic state

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
+/* This is the main client file for pubg.io
+ * @author TakLee96 / jiahang_li@outlook.com
+ **/
+
 const socket = io();
 
 socket.on('connect', function () {
@@ -5,4 +9,33 @@ socket.on('connect', function () {
   socket.on('disconnect', function () {
     console.log('remote disconnected');
   });
+  
+  socket.on('t24b.addPlayer.response', ({ error, map, time }) => {
+    if (error) return console.error(error);
+    console.log('t24b.addPlayer.response', map, time);
+  });
+  
+  socket.on('t24b.action.response', ({ error, map, time }) => {
+    if (error) return console.error(error);
+    console.log('t24b.action.response', map, time);
+  })
 });
+
+function login() {
+  const form = document.getElementById('loginForm');
+  const username = form.username.value;
+  const color = form.color.value;
+  
+  if (username === '') {
+    alert('Pick a cooler username!');
+    return false;
+  }
+  
+  if (color === '#000000') {
+    alert("Don't just pick default black for color!");
+    return false;
+  }
+  
+  socket.emit('t24b.addPlayer', { username, color })
+  return false;
+}
--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const io = require('socket.io');
+const state = require('./lib/state');
 
 const app = express();
 const server = http.createServer(app);
 const socket = io(server);
 
 // helper functions
-function log(msg) {
-  console.log(`[${new Date().toISOString()}] ${msg}`);
+function log(client, msg) {
+  console.log(`[${new Date().toISOString()}] ${[client.conn.id]} ${msg}`);
 }
 
 // static routes
@@ -21,23 +22,30 @@ app.get('/', (req, res) => {
   res.sendFile('public/index.html', { root: __dirname });
 });
 
-app.get('/socket.io.dev.js', (req, res) => {
-  res.sendFile('node_modules/socket.io-client/dist/socket.io.dev.js', { root: __dirname });
-});
-
-app.get('/socket.io.dev.js.map', (req, res) => {
-  res.sendFile('node_modules/socket.io-client/dist/socket.io.dev.js.map', { root: __dirname });
-});
-
 app.use(express.static('public'));
+app.use(express.static('node_modules/socket.io-client/dist'));
+app.use(express.static('lib'));
 
 // socket.io messages
+const t24b = new state();
 socket.on('connection', (client) => {
   const conn = client.conn;
-  log(`client ${conn.id}@${conn.remoteAddress} connected`);
+  log(client, `${conn.remoteAddress} connected`);
   
   client.on('disconnect', () => {
-    log(`client ${conn.id}@${conn.remoteAddress} disconnected`);
+    log(client, `${conn.remoteAddress} disconnected`);
+    t24b.removePlayer(client);
+  });
+  
+  client.on('t24b.addPlayer', (player) => {
+    log(client, `t24b.addPlayer ${JSON.stringify(player)}`);
+    t24b.addPlayer(client, player);
+    client.emit('t24b.addPlayer.response', { map: t24b.map, time: new Date() });
+  });
+  client.on('t24b.action', (action) => {
+    log(client, `t24b.action ${JSON.stringify(action)}`);
+    t24b.action(client, action);
+    client.emit('t24b.action.response', { map: t24b.map, time: new Date() });
   });
 });
 
